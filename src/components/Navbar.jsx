70a4ddb8b26e044a56bc8cf513189edce9363ebd
Ruby Tam <pkamkam@gmail.com>
2024-03-25 18:55:11 +0900

    omamori showcase react created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+function Navbar() {
+  return (
+    <div className="d-flex justify-content-between align-items-center w-100 py-2 px-4 bg-success-subtle">
+      <div className="d-flex align-items-center">
+        <img height="56" className="me-3" src={logo} alt="keyboard and matcha logo" />
+        <h2>Matcha and keyboard</h2>
+      </div>
+      <div className="d-flex align-items-center gap-5">
+        {/* TODO: build the searchbar that filters cafes */}
+        <div className="dropdown">
+          <button type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i className="fa-solid fa-bars"></i>
+          </button>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Profile</a></li>
+            <li><a className="dropdown-item" href="#">Log out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
